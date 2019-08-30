efc60ab1547a2186eb37a592be7d1c01e9d77634
Dominik Boehi <dominik.boehi@gmail.com>
2019-08-31 01:04:55 +0200

    Add initial struct for handling debug adapter protocol

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,75 @@ use debugserver_types::Request;
 use std::fs::File;
 use std::io;
 use std::io::{Read, Write};
+use std::io::{BufRead, BufReader};
 
 use std::env;
+use std::str;
 
 use debugserver_types::{InitializeRequest, InitializeResponse, Capabilities};
 use serde_json;
 
 use log::debug;
 use simplelog::*;
+
+struct DebugAdapter<R: Read, W: Write> {
+    input: BufReader<R>,
+    output: W,
+}
+
+impl<R: Read, W: Write> DebugAdapter<R,W> {
+    fn new(input: R, output: W) -> DebugAdapter<R,W> {
+        DebugAdapter {
+            input: BufReader::new(input),
+            output
+        }
+    }
+
+    fn receive_data(&mut self) -> Result<Vec<u8>, io::Error> {
+        let mut header = String::new();
+
+        self.input.read_line(&mut header)?;
+        debug!("< {}", header.trim_end());
+
+        // we should read an empty line here
+        let mut buff = String::new();
+        self.input.read_line(&mut buff)?;
+
+        let len = get_content_len(&header);
+
+        let mut content = vec![0u8; len];
+        let bytes_read = self.input.read(&mut content)?;
+
+        assert!(bytes_read == len);
+
+        Ok(content)
+
+    }
+
+    fn send_data(&mut self, raw_data: &[u8]) -> Result<(), io::Error> {
+        let response_body = raw_data;
+
+        let response_header = format!("Content-Length: {}\r\n\r\n", response_body.len());
+
+        debug!("> {}", response_header.trim_end());
+        debug!("> {}", str::from_utf8(response_body).unwrap());
+
+        self.output.write(response_header.as_bytes())?;
+        self.output.write(response_body)?;
+
+        Ok(())
+    }
+}
+
+
+fn get_content_len(header: &str) -> usize {
+    let mut parts = header.trim_end().split_ascii_whitespace();
+
+    // discard first part
+    parts.next().unwrap();
+
+    parts.next().unwrap().parse::<usize>().unwrap()
+}
 
 
 fn main() -> std::io::Result<()> {
@@ -28,24 +89,11 @@ fn main() -> std::io::Result<()> {
     debug!("Debugger started in directory {}", current_dir.display());
     debug!("Arguments: {}", args.join(" "));
 
-    // Simulate staying open, to see if something interesting happens
-    let mut header = String::new();
+    let mut adapter = DebugAdapter::new(io::stdin(), io::stdout());
 
-    io::stdin().read_line(&mut header)?;
-    debug!("< {}", header.trim_end());
+    let data = adapter.receive_data()?;
+    let req: InitializeRequest = serde_json::from_slice(&data).unwrap();
 
-    // we should read an empty line here
-    let mut buff = String::new();
-    io::stdin().read_line(&mut buff)?;
-
-    let len = get_content_len(&header);
-
-    let mut content = vec![0u8; len];
-    let bytes_read = io::stdin().read(&mut content)?;
-
-    assert!(bytes_read == len);
-
-    let req: InitializeRequest = serde_json::from_slice(&content).unwrap();
     debug!("< {:?}", req);
 
     let init_resp = InitializeResponse {
@@ -63,14 +111,8 @@ fn main() -> std::io::Result<()> {
 
     let response_body = serde_json::to_vec(&init_resp).unwrap();
 
-    let response_header = format!("Content-Length: {}\r\n\r\n", response_body.len());
+    adapter.send_data(&response_body)?;
 
-    let mut stdout = io::stdout();
-
-    debug!("> {}", response_header.trim_end());
-    debug!("> {:?}", init_resp);
-    stdout.write(&response_header.as_bytes())?;
-    stdout.write(&response_body)?;
 
     let init_evt = InitializedEvent {
         seq: 2,
@@ -83,42 +125,17 @@ fn main() -> std::io::Result<()> {
 
     let response_body = serde_json::to_vec(&init_evt).unwrap();
 
-    let response_header = format!("Content-Length: {}\r\n\r\n", response_body.len());
-
-    debug!("> {}", response_header.trim_end());
-    debug!("> {:?}", init_evt);
-    stdout.write(&response_header.as_bytes())?;
-    stdout.write(&response_body)?;
+    adapter.send_data(&response_body)?;
     
     // look for other request
-
     loop {
-        let mut header = String::new();
-
-        io::stdin().read_line(&mut header)?;
-        debug!("< {}", header.trim_end());
-
-        // we should read an empty line here
-        let mut buff = String::new();
-        io::stdin().read_line(&mut buff)?;
-
-        let len = get_content_len(&header);
-
-        let mut content = vec![0u8; len];
-        let bytes_read = io::stdin().read(&mut content)?;
-
-        assert!(bytes_read == len);
+        let content = adapter.receive_data()?;
 
         let req: Request = serde_json::from_slice(&content).unwrap();
         debug!("< {:?}", req);
     }
+
+
+    Ok(())
 }
 
-fn get_content_len(header: &str) -> usize {
-    let mut parts = header.trim_end().split_ascii_whitespace();
-
-    // discard first part
-    parts.next().unwrap();
-
-    parts.next().unwrap().parse::<usize>().unwrap()
-}
