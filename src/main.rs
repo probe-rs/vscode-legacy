6821c6b458c34299ad0bf05db2a05bd4caf49d33
Dominik Boehi <dominik.boehi@gmail.com>
2019-08-30 21:31:06 +0200

    Use simple_log and log crates for logging

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,37 @@
 use std::fs::File;
 use std::io;
 use std::io::Read;
-use std::io::Write;
 
 use std::env;
 
 use debugserver_types::InitializeRequest;
 use serde_json;
 
-struct Logger {
-    file: Option<File>,
-}
+use log::debug;
+use simplelog::*;
 
-impl Logger {
-    fn from_env_var(var_name: &str) -> io::Result<Logger> {
-        let file = match env::var(var_name) {
-            Ok(path) => Some(File::create(path)?),
-            Err(_) => None,
-        };
-
-        Ok(Logger { file })
-    }
-}
-
-impl Write for Logger {
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        match self.file {
-            Some(ref mut f) => f.write(buf),
-            None => Ok(buf.len()),
-        }
-    }
-
-    fn flush(&mut self) -> io::Result<()> {
-        match self.file {
-            Some(ref mut f) => f.flush(),
-            None => Ok(()),
-        }
-    }
-}
 
 fn main() -> std::io::Result<()> {
-    let mut logger = Logger::from_env_var("PROBE_RS_LOGFILE")?;
-    let current_dir = env::current_dir()?;
+
+    if let Ok(path) = env::var("PROBE_RS_LOGFILE") {
+        let file = File::create(path)?;
+
+        // Ignore error setting up the debugger
+        let _ = WriteLogger::init(LevelFilter::Debug, Config::default(), file);
+    }
+
     let args: Vec<String> = env::args().collect();
+    let current_dir = env::current_dir()?;
 
-    writeln!(
-        logger,
-        "Debugger started in directory {}",
-        current_dir.display()
-    )?;
-
-    writeln!(logger, "Arguments: {}", args.join(" "))?;
+    debug!("Debugger started in directory {}", current_dir.display());
+    debug!("Arguments: {}", args.join(" "));
 
     // Simulate staying open, to see if something interesting happens
 
     let mut header = String::new();
 
     io::stdin().read_line(&mut header)?;
-    writeln!(logger, "< {}", header.trim_end())?;
+    debug!("< {}", header.trim_end());
 
     // we should read an empty line here
     let mut buff = String::new();
@@ -71,12 +45,13 @@ fn main() -> std::io::Result<()> {
     assert!(bytes_read == len);
 
     let req: InitializeRequest = serde_json::from_slice(&content).unwrap();
-    writeln!(logger, "< {:?}", req)?;
+    debug!("< {:?}", req);
 
     loop {
         let mut buff = String::new();
         io::stdin().read_line(&mut buff)?;
-        writeln!(logger, "< {}", buff.trim_end())?;
+
+        debug!("< {}", buff.trim_end());
     }
 }
 
